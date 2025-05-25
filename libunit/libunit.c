bbf83e57e953c6f6c373a3a7cac9738f17688704
Andrei Enshin <aenshin@c3r2s7.42tokyo.jp>
2025-05-25 22:26:49 +0900

    norm

--- libunit/libunit.c
+++ libunit/libunit.c
@@ -6,9 +6,12 @@
 /*   By: aenshin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 22:21:53 by aenshin           #+#    #+#             */
-/*   Updated: 2025/05/25 22:22:19 by aenshin          ###   ########.fr       */
+/*   Updated: 2025/05/25 22:26:40 by aenshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+//TODO: remove dbg prints
+//TODO: error handling like malloc, etc
 
 #include "./libunit.h"
 #include "./ft_printf/ft_printf.h"
@@ -20,20 +23,19 @@
 void	load_test(t_unit_test *test_list, char *test_name, int (*test_f)(void))
 {
 	ft_printf("(dbg) loading test..\n");
-	while(test_list->next != NULL)
+	while (test_list->next != NULL)
 		test_list = test_list->next;
-	
-	if (test_list->name != NULL) {
+	if (test_list->name != NULL)
+	{
 		test_list->next = malloc(sizeof(t_unit_test));
 		test_list = test_list->next;
 	}
-
 	test_list->name = test_name;
 	test_list->test_f = test_f;
 	test_list->next = NULL;
 }
 
-void child(t_unit_test *t)
+void	child(t_unit_test *t)
 {
 	int	test_res;
 
@@ -43,15 +45,15 @@ void child(t_unit_test *t)
 	exit(1);
 }
 
-char	*decode_status(int	status)
+char	*decode_status(int status)
 {
-	if(WIFEXITED(status))
+	if (WIFEXITED(status))
 	{
 		ft_printf("(dbg) i'm in exit!\n");
 		if (WEXITSTATUS(status) == 0)
-			return "OK";
+			return ("OK");
 		else
-			return "KO";
+			return ("KO");
 	}
 	else
 	{
@@ -61,59 +63,48 @@ char	*decode_status(int	status)
 		if (WTERMSIG(status) == SIGBUS)
 			return ("SIGBUS");
 	}
-	return "WTF :)";
+	return ("WTF :)");
 }
 
-int parent(char *f_name, char *t_name)
+int	parent(char *f_name, char *t_name)
 {
 	int	status;
 	int	ret;
-	// TODO: get status - from exit code or signal
 
 	ret = 0;
 	wait(&status);
-
 	ft_printf("(dbg) status [%d]\n", status);
 	ft_printf("[%s]:[%s]:", f_name, t_name);
-
 	if (status == 0)
 		ret = 1;
 	ft_printf("[%s]\n", decode_status(status));
-
 	return (ret);
 }
 
-int		launch_test(t_unit_test *test_list, char *test_func_name) {
-	int res;
-	int	test_cnt;
-	int	succeed_test;
+int	launch_test(t_unit_test *test_list, char *test_func_name)
+{
 	pid_t	pid;
+	int		res;
+	int		test_cnt;
+	int		succeed_test;
 
 	res = 0;
 	test_cnt = 0;
 	succeed_test = 0;
-	
-	while(test_list != NULL)
+	while (test_list != NULL)
 	{
 		pid = fork();
-		// TODO: what it means?
 		if (pid < 0)
 			return (-1);
 		else if (pid == 0)
 			child(test_list);
 		else if (pid > 0)
 			succeed_test += parent(test_func_name, test_list->name);
-
 		test_cnt++;
-		// TODO: fork and check here exit status or signal
-		//res = test_list->test_f();
-			
 		test_list = test_list->next;
 	}
-
-	if (test_cnt == succeed_test) 
+	if (test_cnt == succeed_test)
 		return (0);
 	else
 		return (-1);
 }
-
