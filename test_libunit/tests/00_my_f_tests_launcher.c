355ae8d97647a0199a5e9328e54c25506fbd8fea
Andrei Enshin <aenshin@c3r2s7.42tokyo.jp>
2025-05-25 23:02:39 +0900

    norm

--- test_libunit/tests/00_my_f_tests_launcher.c
+++ test_libunit/tests/00_my_f_tests_launcher.c
@@ -1,20 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   00_my_f_tests_launcher.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aenshin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/25 23:00:56 by aenshin           #+#    #+#             */
+/*   Updated: 2025/05/25 23:01:28 by aenshin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../libunit/libunit.h"
 #include "tests.h"
 #include <stdlib.h>
 
-int my_f_tests_launcher(void)
+int	my_f_tests_launcher(void)
 {
 	t_unit_test	*t;
-	int	res;
+	int			res;
 
-	t = malloc(sizeof(t_unit_test)); //TODO: err handling
+	t = malloc(sizeof(t_unit_test));
 	t->name = NULL;
 	load_test(t, "my_f_test_ok", &my_f_test_ok);
 	load_test(t, "my_f_test_ko", &my_f_test_ko);
 	load_test(t, "my_f_test_sigsegv", &my_f_test_sigsegv);
 	load_test(t, "my_f_test_sigbus", &my_f_test_sigbus);
-
 	res = launch_test(t, "my_f");
-	//TODO: do we need to free list or launch can free?
 	return (res);
 }
--- test_libunit/tests/tests.h
+++ test_libunit/tests/tests.h
@@ -1,7 +1,21 @@
-int my_f_tests_launcher(void);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tests.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aenshin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/25 23:01:37 by aenshin           #+#    #+#             */
+/*   Updated: 2025/05/25 23:02:32 by aenshin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef TESTS_H
+# define TESTS_H
+
+int	my_f_tests_launcher(void);
 int	my_f_test_ok(void);
 int	my_f_test_ko(void);
-int my_f_test_sigsegv(void);
-int my_f_test_sigbus(void);
-
+int	my_f_test_sigsegv(void);
+int	my_f_test_sigbus(void);
+#endif
