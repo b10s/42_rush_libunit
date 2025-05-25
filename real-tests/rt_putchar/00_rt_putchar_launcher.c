b64081932076a30ed5e19ffe362fac393fcf9ebf
Andrei Enshin <aenshin@c3r2s7.42tokyo.jp>
2025-05-25 23:26:39 +0900

    aaa

--- real-tests/rt_putchar/00_rt_putchar_launcher.c
+++ real-tests/rt_putchar/00_rt_putchar_launcher.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rt_putchar.h                                       :+:      :+:    :+:   */
+/*   00_rt_putchar_launcher.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aenshin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 23:16:43 by aenshin           #+#    #+#             */
-/*   Updated: 2025/05/25 23:16:45 by aenshin          ###   ########.fr       */
+/*   Updated: 2025/05/25 23:24:19 by aenshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,12 @@
 
 int	rt_putchar_launcher(void)
 {
-	t_unit_test	*lst;
+	t_unit_test	*t;
 	int			ret;
 
-	ft_printf("rt_putchar:\n");
-	lst = NULL;
-	load_test(&lst, "basic_test", &basic_test);
-	load_test(&lst, "unprintable_char_test", &unprintable_char_test);
-	ret = launch_tests(&lst);
-	clean_list(&lst);
+	t = malloc(sizeof(t_unit_test));
+	load_test(t, "basic_test", &basic_test);
+	load_test(t, "unprintable_char_test", &unprintable_char_test);
+	ret = launch_test(t, "rt_putchar");
 	return (ret);
 }
